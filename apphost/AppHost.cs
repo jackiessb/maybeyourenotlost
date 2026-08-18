d72f8e10448ace51bb8a458512a3edee0a703f02
Jackson Torregrossa <memjkt@gmail.com>
2026-08-18 13:51:03 -0400

    feat: set up azd deploy

--- apphost/AppHost.cs
+++ apphost/AppHost.cs
@@ -11,10 +11,18 @@ var encouragementApi = builder.AddProject<Projects.encouragement_api>("encourage
 var contactsApi = builder.AddProject<Projects.contacts_api>("contacts-api")
     .WithReference(contactsDb);
 
-builder.AddJavaScriptApp("frontend", "../frontend", "dev")
+#pragma warning disable ASPIREJAVASCRIPT001
+var frontend = builder.AddJavaScriptApp("frontend", "../frontend", "dev")
     .WithHttpEndpoint(env: "PORT")
     .WithExternalHttpEndpoints()
     .WithReference(encouragementApi)
-    .WithReference(contactsApi);
+    .WithReference(contactsApi)
+    .WithBuildScript("build")
+    .PublishAsStaticWebsite("/contacts", contactsApi)
+    .PublishAsStaticWebsite("/encouragements", encouragementApi);
+#pragma warning restore ASPIREJAVASCRIPT001
+
+encouragementApi.WithEnvironment("Frontend__Origin", frontend.GetEndpoint("http"));
+contactsApi.WithEnvironment("Frontend__Origin", frontend.GetEndpoint("http"));
 
 builder.Build().Run();
--- backend/encouragement-api/Program.cs
+++ backend/encouragement-api/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.AddServiceDefaults();
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
